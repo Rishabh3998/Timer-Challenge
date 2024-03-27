8a56926de571c6e9de8ade79e5caccf132c214d1
Rishabh Kumar <rsk1998200@gmail.com>
2024-03-27 13:01:42 +0530

    refactor

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,32 +1,38 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(({ result, targetTime }, ref) => {
-  // Forwarding ref
-  const dialog = useRef();
+const ResultModal = forwardRef(
+  ({ targetTime, remainingTime, onReset }, ref) => {
+    // Forwarding ref
+    const dialog = useRef();
+    const userLost = remainingTime <= 0;
+    const formattedTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round(1 - (remainingTime / (targetTime * 1000)) * 100);
 
-  useImperativeHandle(ref, () => {
-    return {
-      open() {
-        dialog.current.showModal();
-      },
-    };
-  });
+    useImperativeHandle(ref, () => {
+      return {
+        open() {
+          dialog.current.showModal();
+        },
+      };
+    });
 
-  return (
-    <dialog className="result-modal" ref={dialog}>
-      <h2>Your {result} </h2>
-      <p>
-        The target time was
-        <strong>{targetTime} seconds.</strong>
-      </p>
-      <p>
-        You stopped the timer with <strong>X seconds left.</strong>
-      </p>
-      <form method="dialog">
-        <button>Close</button>
-      </form>
-    </dialog>
-  );
-});
+    return (
+      <dialog className="result-modal" ref={dialog}>
+        {userLost ? <h2>You lost</h2> : <h2>You win score: {score}</h2>}
+        <p>
+          The target time was
+          <strong>{targetTime} seconds.</strong>
+        </p>
+        <p>
+          You stopped the timer with{" "}
+          <strong>{formattedTime} seconds left.</strong>
+        </p>
+        <form method="dialog" onSubmit={onReset}>
+          <button>Close</button>
+        </form>
+      </dialog>
+    );
+  }
+);
 
 export default ResultModal;
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -4,35 +4,48 @@ import ResultModal from "./ResultModal";
 const TimerChallenge = ({ title, targetTime }) => {
   const timerId = useRef();
   const dialog = useRef();
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const timerIsActive = timeRemaining > 0 && targetTime < 1000;
+  if (timeRemaining <= 0) {
+    clearInterval(timerId.current);
+    setTimeRemaining(targetTime * 1000);
+    dialog.current.open();
+  }
   const handleClick = () => {
-    timerId.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.open();
-    }, targetTime * 1000);
-    setTimerStarted(true);
+    timerId.current = setInterval(() => {
+      setTimeRemaining((prev) => prev - 10);
+    }, 10);
+  };
+
+  const handleReset = () => {
+    setTimeRemaining(targetTime * 1000);
   };
 
   const handleStop = () => {
-    clearTimeout(timerId.current);
+    dialog.current.open();
+    clearInterval(timerId.current);
   };
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleClick}>
-            {timerStarted ? "Stop" : "Start"} challenge
+          <button onClick={timerIsActive ? handleStop : handleClick}>
+            {timerIsActive ? "Stop" : "Start"} challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          Timer is {timerStarted ? "running..." : "expired"}
+        <p className={timerIsActive ? "active" : undefined}>
+          Timer is {timerIsActive ? "running..." : "expired"}
         </p>
       </section>
     </>
